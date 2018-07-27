6bbf31016f67d6e1b63ac7cc6a29998c60d2f4b3
bothadeon <deon.botha@modusbox.com>
2018-07-27 14:02:44 +0200

    Added succesful unit test for /settlementWindows

--- handlers/settlementWindows.js
+++ handlers/settlementWindows.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Boom = require('boom');
+const dataAccess = require('../data/settlementWindows');
 
 /**
  * Operations on /settlementWindows
@@ -8,12 +9,40 @@ const Boom = require('boom');
 module.exports = {
     /**
      * summary: Returns a Settlement Window(s) as per parameter(s).
-     * description: 
+     * description:
      * parameters: id, state, fromDateTime, toDateTime
      * produces: application/json
      * responses: 200, 400, 401, 404, 415, default
      */
-    get: function getSettlementWindowByParams(request, h) {
-        return Boom.notImplemented();
+    get: async function getSettlementWindowByParams(request, h) {
+        const getData = new Promise((resolve, reject) => {
+            switch (request.server.app.responseCode) {
+                case 200:
+                case 400:
+                case 401:
+                case 404:
+                case 415:
+                    dataAccess.get[`${request.server.app.responseCode}`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+                    break
+                default:
+                    dataAccess.get[`default`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+            }
+
+        })
+        try {
+            return await getData
+        } catch (e) {
+            throw (Boom.boomify(e))
+        }
     }
 };
--- tests/unit/settlementWindows.js
+++ tests/unit/settlementWindows.js
@@ -5,6 +5,7 @@ const Hapi = require('hapi');
 const HapiOpenAPI = require('hapi-openapi');
 const Path = require('path');
 const Mockgen = require('../../data/mockgen.js');
+const responseCodes = [200, 400, 401, 404, 415];
 
 /**
  * Test for /settlementWindows
@@ -13,7 +14,7 @@ Test('/settlementWindows', function (t) {
 
     /**
      * summary: Returns a Settlement Window(s) as per parameter(s).
-     * description: 
+     * description:
      * parameters: id, state, fromDateTime, toDateTime
      * produces: application/json
      * responses: 200, 400, 401, 404, 415, default
@@ -21,55 +22,60 @@ Test('/settlementWindows', function (t) {
     t.test('test getSettlementWindowByParams get operation', async function (t) {
 
         const server = new Hapi.Server();
-
-        await server.register({
-            plugin: HapiOpenAPI,
-            options: {
-                api: Path.resolve(__dirname, '../config/swagger.json'),
-                handlers: Path.join(__dirname, '../handlers'),
-                outputvalidation: true
-            }
-        });
-
-        const requests = new Promise((resolve, reject) => {
-            Mockgen().requests({
-                path: '/settlementWindows',
-                operation: 'get'
-            }, function (error, mock) {
-                return error ? reject(error) : resolve(mock);
+        try {
+            await server.register({
+                plugin: HapiOpenAPI,
+                options: {
+                    api: Path.resolve(__dirname, '../../config/swagger.json'),
+                    handlers: Path.join(__dirname, '../../handlers'),
+                    outputvalidation: true
+                }
             });
-        });
 
-        const mock = await requests;
+            const requests = new Promise((resolve, reject) => {
+                Mockgen().requests({
+                    path: '/settlementWindows',
+                    operation: 'get'
+                }, function (error, mock) {
+                    return error ? reject(error) : resolve(mock);
+                });
+            });
 
-        t.ok(mock);
-        t.ok(mock.request);
-        //Get the resolved path from mock request
-        //Mock request Path templates({}) are resolved using path parameters
-        const options = {
-            method: 'get',
-            url: '/v1' + mock.request.path
-        };
-        if (mock.request.body) {
-            //Send the request body
-            options.payload = mock.request.body;
-        } else if (mock.request.formData) {
-            //Send the request form data
-            options.payload = mock.request.formData;
-            //Set the Content-Type as application/x-www-form-urlencoded
-            options.headers = options.headers || {};
-            options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+            const mock = await requests;
+
+            t.ok(mock);
+            t.ok(mock.request);
+            //Get the resolved path from mock request
+            //Mock request Path templates({}) are resolved using path parameters
+            const options = {
+                method: 'get',
+                url: '/v1' + mock.request.path
+            };
+            if (mock.request.body) {
+                //Send the request body
+                options.payload = mock.request.body;
+            } else if (mock.request.formData) {
+                //Send the request form data
+                options.payload = mock.request.formData;
+                //Set the Content-Type as application/x-www-form-urlencoded
+                options.headers = options.headers || {};
+                options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+            }
+            // If headers are present, set the headers.
+            if (mock.request.headers && mock.request.headers.length > 0) {
+                options.headers = mock.request.headers;
+            }
+
+            for (let responseCode of responseCodes) {
+                server.app.responseCode = responseCode
+                const response = await server.inject(options);
+                t.equal(response.statusCode, responseCode, 'Ok response status');
+            }
+            t.end();
+        } catch (e) {
+            t.fail(e);
+            t.end()
         }
-        // If headers are present, set the headers.
-        if (mock.request.headers && mock.request.headers.length > 0) {
-            options.headers = mock.request.headers;
-        }
-
-        const response = await server.inject(options);
-
-        t.equal(response.statusCode, 200, 'Ok response status');
-        t.end();
-
     });
-    
+
 });
