0e5b3bd850dc61ab9def8418c0e74745194fecbf
bothadeon <deon.botha@modusbox.com>
2018-07-30 10:40:52 +0200

    Added more unit tests

--- handlers/settlements/{Id}.js
+++ handlers/settlements/{Id}.js
@@ -1,29 +1,86 @@
 'use strict';
 
 const Boom = require('boom');
+const Path = require('path');
+const dataAccess = require('../../data/settlements/{Id}');
 
+console.log('path ', Path.basename(__filename));
 /**
  * Operations on /settlements/{Id}
  */
 module.exports = {
     /**
      * summary: Returns Settlement(s) as per parameters/filter criteria.
-     * description: 
+     * description:
      * parameters: Id, currency, Id
      * produces: application/json
      * responses: 200, 400, 401, 404, 415, default
      */
-    get: function getSettlementsBySettlementParticipantCurrency(request, h) {
-        return Boom.notImplemented();
+    get: async function getSettlementsBySettlementParticipantCurrency(request, h) {
+        const getData = new Promise((resolve, reject) => {
+            switch (request.server.app.responseCode) {
+                case 200:
+                case 400:
+                case 401:
+                case 404:
+                case 415:
+                    dataAccess.get[`${request.server.app.responseCode}`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+                    break
+                default:
+                    dataAccess.get[`default`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+            }
+        })
+        try {
+            return await getData
+        } catch (e) {
+            throw (Boom.boomify(e))
+        }
     },
     /**
      * summary: Acknowledegement of settlement by updating with Settlements Id.
-     * description: 
+     * description:
      * parameters: Id, settlementUpdatePayload
      * produces: application/json
      * responses: 200, 400, 401, 404, 415, default
      */
-    put: function updateSettlementBySettlementId(request, h) {
-        return Boom.notImplemented();
+    put: async function updateSettlementBySettlementId(request, h) {
+        const getData = new Promise((resolve, reject) => {
+            switch (request.server.app.responseCode) {
+                case 200:
+                case 400:
+                case 401:
+                case 404:
+                case 415:
+                    dataAccess.put[`${request.server.app.responseCode}`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+                    break
+                default:
+                    dataAccess.put[`default`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+            }
+        })
+        try {
+            return await getData
+        } catch (e) {
+            throw (Boom.boomify(e))
+        }
     }
 };
