fbfc225473a356dd48079c546d66bb0829b3e7de
Valentin Genev <vgenev@gmail.com>
2018-07-30 10:56:44 +0200

    Added last unit tests

--- handlers/settlements/{settlementId}/participants/{participantId}/accounts/{accountId}.js
+++ handlers/settlements/{settlementId}/participants/{participantId}/accounts/{accountId}.js
@@ -1,29 +1,86 @@
 'use strict';
 
 const Boom = require('boom');
+const Path = require('path');
+const dataAccess = require('../../../../../../data/settlements/{settlementId}/participants/{participantId}/accounts/{accountId}');
 
+console.log('path ', Path.basename(__filename));
 /**
  * Operations on /settlements/{settlementId}/participants/{participantId}/accounts/{accountId}
  */
 module.exports = {
     /**
      * summary: Returns Settlement(s) as per filter criteria.
-     * description: 
+     * description:
      * parameters: settlementId, participantId, accountId
      * produces: application/json
      * responses: 200, 400, 401, 404, 415, default
      */
-    get: function getSettlementsBySettlementParticipantAccounts(request, h) {
-        return Boom.notImplemented();
+    get: async function getSettlementsBySettlementParticipantAccounts(request, h) {
+        const getData = new Promise((resolve, reject) => {
+            switch (request.server.app.responseCode) {
+                case 200:
+                case 400:
+                case 401:
+                case 404:
+                case 415:
+                    dataAccess.get[`${request.server.app.responseCode}`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+                    break
+                default:
+                    dataAccess.get[`default`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+            }
+        })
+        try {
+            return await getData
+        } catch (e) {
+            throw (Boom.boomify(e))
+        }
     },
     /**
      * summary: Acknowledegement of settlement by updating thereason and state by Settlements Id, Participant Id and accounts Id
-     * description: 
+     * description:
      * parameters: settlementId, participantId, accountId, settlementUpdatePayload
      * produces: application/json
      * responses: 200, 400, 401, 404, 415, default
      */
-    put: function updateSettlementBySettlementParticipantsAccounts(request, h) {
-        return Boom.notImplemented();
+    put: async function updateSettlementBySettlementParticipantsAccounts(request, h) {
+        const getData = new Promise((resolve, reject) => {
+            switch (request.server.app.responseCode) {
+                case 200:
+                case 400:
+                case 401:
+                case 404:
+                case 415:
+                    dataAccess.put[`${request.server.app.responseCode}`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+                    break
+                default:
+                    dataAccess.put[`default`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+            }
+        })
+        try {
+            return await getData
+        } catch (e) {
+            throw (Boom.boomify(e))
+        }
     }
 };
