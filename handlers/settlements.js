a5554ea2087073703b44290c0f34a94e40c506b8
bothadeon <deon.botha@modusbox.com>
2018-08-02 14:05:44 +0200

    Added POST operation to settlements/

--- handlers/settlements.js
+++ handlers/settlements.js
@@ -31,7 +31,6 @@
  ******/
 
 'use strict';
-
 const dataAccess = require('../data/settlements');
 const Boom = require('boom');
 const Logger = require('@mojaloop/central-services-shared').Logger
@@ -67,6 +66,44 @@ module.exports = {
                     break
                 default:
                     dataAccess.get[`default`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+            }
+
+        })
+        try {
+            return await getData
+        } catch (e) {
+            throw (Boom.boomify(e))
+        }
+    },
+    /**
+     * summary: Trigger the creation of a settlement event, that does the calculation of the net settlement position per participant and marks all transfers in the affected windows as Pending settlement. Returned dataset is the net settlement report for the settlementwindow
+     * description: 
+     * parameters: settlementEventPayload
+     * produces: application/json
+     * responses: 200, 400, 401, 404, 415, default
+     */
+    post: async function postSettlementEvent(request, h) {
+        const getData = new Promise((resolve, reject) => {
+            switch (request.server.app.responseCode) {
+                case 200:
+                case 400:
+                case 401:
+                case 404:
+                case 415:
+                    dataAccess.post[`${request.server.app.responseCode}`](request, h, (error, mock) => {
+                        if (error) reject(error)
+                        else if (!mock.responses) resolve()
+                        else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
+                        else resolve(mock.responses)
+                    })
+                    break
+                default:
+                    dataAccess.post[`default`](request, h, (error, mock) => {
                         if (error) reject(error)
                         else if (!mock.responses) resolve()
                         else if (mock.responses && mock.responses.code) resolve(Boom.boomify(new Error(mock.responses.message), {statusCode: mock.responses.code}))
---
+++ tests/settlements/{settlementId}/participants/{participantId}/accounts/{accountId}.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const Test = require('tape');
+const Hapi = require('hapi');
+const HapiOpenAPI = require('hapi-openapi');
+const Path = require('path');
+const Mockgen = require('../../../../../../data/mockgen.js');
+
+/**
+ * Test for /settlements/{settlementId}/participants/{participantId}/accounts/{accountId}
+ */
+Test('/settlements/{settlementId}/participants/{participantId}/accounts/{accountId}', function (t) {
+
+    /**
+     * summary: Returns Settlement(s) as per filter criteria.
+     * description: 
+     * parameters: settlementId, participantId, accountId
+     * produces: application/json
+     * responses: 200, 400, 401, 404, 415, default
+     */
+    t.test('test getSettlementsBySettlementParticipantAccounts get operation', async function (t) {
+
+        const server = new Hapi.Server();
+
+        await server.register({
+            plugin: HapiOpenAPI,
+            options: {
+                api: Path.resolve(__dirname, '../../../../../../config/swagger.json'),
+                handlers: Path.join(__dirname, '../../../../../../handlers'),
+                outputvalidation: true
+            }
+        });
+
+        const requests = new Promise((resolve, reject) => {
+            Mockgen().requests({
+                path: '/settlements/{settlementId}/participants/{participantId}/accounts/{accountId}',
+                operation: 'get'
+            }, function (error, mock) {
+                return error ? reject(error) : resolve(mock);
+            });
+        });
+
+        const mock = await requests;
+
+        t.ok(mock);
+        t.ok(mock.request);
+        //Get the resolved path from mock request
+        //Mock request Path templates({}) are resolved using path parameters
+        const options = {
+            method: 'get',
+            url: '/v1' + mock.request.path
+        };
+        if (mock.request.body) {
+            //Send the request body
+            options.payload = mock.request.body;
+        } else if (mock.request.formData) {
+            //Send the request form data
+            options.payload = mock.request.formData;
+            //Set the Content-Type as application/x-www-form-urlencoded
+            options.headers = options.headers || {};
+            options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        }
+        // If headers are present, set the headers.
+        if (mock.request.headers && mock.request.headers.length > 0) {
+            options.headers = mock.request.headers;
+        }
+
+        const response = await server.inject(options);
+
+        t.equal(response.statusCode, 200, 'Ok response status');
+        t.end();
+
+    });
+    /**
+     * summary: Acknowledegement of settlement by updating the reason and state by Settlements Id, Participant Id and accounts Id.
+     * description: 
+     * parameters: settlementId, participantId, accountId, settlementParticipantAccountUpdate
+     * produces: application/json
+     * responses: 200, 400, 401, 404, 415, default
+     */
+    t.test('test updateSettlementBySettlementParticipantsAccounts put operation', async function (t) {
+
+        const server = new Hapi.Server();
+
+        await server.register({
+            plugin: HapiOpenAPI,
+            options: {
+                api: Path.resolve(__dirname, '../../../../../../config/swagger.json'),
+                handlers: Path.join(__dirname, '../../../../../../handlers'),
+                outputvalidation: true
+            }
+        });
+
+        const requests = new Promise((resolve, reject) => {
+            Mockgen().requests({
+                path: '/settlements/{settlementId}/participants/{participantId}/accounts/{accountId}',
+                operation: 'put'
+            }, function (error, mock) {
+                return error ? reject(error) : resolve(mock);
+            });
+        });
+
+        const mock = await requests;
+
+        t.ok(mock);
+        t.ok(mock.request);
+        //Get the resolved path from mock request
+        //Mock request Path templates({}) are resolved using path parameters
+        const options = {
+            method: 'put',
+            url: '/v1' + mock.request.path
+        };
+        if (mock.request.body) {
+            //Send the request body
+            options.payload = mock.request.body;
+        } else if (mock.request.formData) {
+            //Send the request form data
+            options.payload = mock.request.formData;
+            //Set the Content-Type as application/x-www-form-urlencoded
+            options.headers = options.headers || {};
+            options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        }
+        // If headers are present, set the headers.
+        if (mock.request.headers && mock.request.headers.length > 0) {
+            options.headers = mock.request.headers;
+        }
+
+        const response = await server.inject(options);
+
+        t.equal(response.statusCode, 200, 'Ok response status');
+        t.end();
+
+    });
+    
+});
