fc62d292faa7845c10895821060f27b4798d16bf
ggrg <georgi.georgiev@modusbox.com>
2019-02-13 13:46:15 +0200

    Finished populate admin test data

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -28,7 +28,6 @@ const Test = require('tapes')(require('tape'))
 const Sinon = require('sinon')
 const Logger = require('@mojaloop/central-services-shared').Logger
 const fetch = require('node-fetch')
-// const ParticipantCurrencyModel = require('../../../../src/models/settlement/participantCurrency')
 // const Db = require('../../../../src/models')
 
 Test('Settlements', async (settlementsTest) => {
@@ -37,8 +36,8 @@ Test('Settlements', async (settlementsTest) => {
   const CENTRAL_LEDGER_ADMIN_HOST = '127.0.0.1'
   const CENTRAL_LEDGER_ADMIN_PORT = 3001
   const CENTRAL_LEDGER_ADMIN_BASE = ''
-  
-
+  const currency = 'USD'
+  const fspList = ['dfsp1', 'dfsp2']
   settlementsTest.beforeEach(test => {
     sandbox = Sinon.createSandbox()
     test.end()
@@ -67,7 +66,7 @@ Test('Settlements', async (settlementsTest) => {
             body: JSON.stringify(body)
           }
           let res = await fetch(url, opts)
-          test.equal(res.status, 201, 'POST request returned 201 Created')
+          test.equal(res.status, 201, 'returned 201 Created')
           test.end()
         } catch (err) {
           Logger.error(`settlementsInitTest failed with error - ${err}`)
@@ -92,13 +91,152 @@ Test('Settlements', async (settlementsTest) => {
             body: JSON.stringify(body)
           }
           let res = await fetch(url, opts)
-          test.equal(res.status, 201, 'POST request returned 201 Created')
+          test.equal(res.status, 201, 'returned 201 Created')
           test.end()
         } catch (err) {
           Logger.error(`settlementsInitTest failed with error - ${err}`)
           test.fail()
           test.end()
         }
+      })
+
+      await settlementsInitTest.test('add participant and participant account', async test => {
+        for (let fsp = 0; fsp < fspList.length; fsp++) {
+          try {
+            let url = `${CENTRAL_LEDGER_ADMIN_URI_PREFIX}://${CENTRAL_LEDGER_ADMIN_HOST}:${CENTRAL_LEDGER_ADMIN_PORT}${CENTRAL_LEDGER_ADMIN_BASE}/participants`
+            let headers = {
+              'Content-Type': 'application/json'
+            }
+            let body = {
+              name: fspList[fsp],
+              currency: currency
+            }
+            let opts = {
+              method: 'POST',
+              headers,
+              body: JSON.stringify(body)
+            }
+            let res = await fetch(url, opts)
+            test.equal(res.status, 201, `returned 201 Created for ${fspList[fsp]}`)
+          } catch (err) {
+            Logger.error(`settlementsInitTest failed with error - ${err}`)
+            test.fail()
+            test.end()
+          }
+        }
+        test.end()
+      })
+
+      await settlementsInitTest.test('add participant account limits', async test => {
+        for (let fsp = 0; fsp < fspList.length; fsp++) {
+          try {
+            let url = `${CENTRAL_LEDGER_ADMIN_URI_PREFIX}://${CENTRAL_LEDGER_ADMIN_HOST}:${CENTRAL_LEDGER_ADMIN_PORT}${CENTRAL_LEDGER_ADMIN_BASE}/participants/${fspList[fsp]}/initialPositionAndLimits`
+            let headers = {
+              'Content-Type': 'application/json'
+            }
+            let body = {
+              currency: currency,
+              limit: {
+                type: 'NET_DEBIT_CAP',
+                value: 1000
+              },
+              initialPosition: 0
+            }
+            let opts = {
+              method: 'POST',
+              headers,
+              body: JSON.stringify(body)
+            }
+            let res = await fetch(url, opts)
+            test.equal(res.status, 201, `returned 201 created limits for ${fspList[fsp]}`)
+          } catch (err) {
+            Logger.error(`settlementsInitTest failed with error - ${err}`)
+            test.fail()
+            test.end()
+          }
+        }
+        test.end()
+      })
+
+      await settlementsInitTest.test('add participant FSPIOP_CALLBACK_URL_TRANSFER_POST endpoint', async test => {
+        let headers = {
+          'Content-Type': 'application/json'
+        }
+        for (let fsp = 0; fsp < fspList.length; fsp++) {
+          try {
+            let url = `${CENTRAL_LEDGER_ADMIN_URI_PREFIX}://${CENTRAL_LEDGER_ADMIN_HOST}:${CENTRAL_LEDGER_ADMIN_PORT}${CENTRAL_LEDGER_ADMIN_BASE}/participants/${fspList[fsp]}/endpoints`
+            let body = {
+              type: 'FSPIOP_CALLBACK_URL_TRANSFER_POST',
+              value: `http://localhost:1080/${fspList[fsp]}/transfers`
+            }
+            let opts = {
+              method: 'POST',
+              headers,
+              body: JSON.stringify(body)
+            }
+            let res = await fetch(url, opts)
+            test.equal(res.status, 201, `returned 201 created endpoint for ${fspList[fsp]}`)
+          } catch (err) {
+            Logger.error(`settlementsInitTest failed with error - ${err}`)
+            test.fail()
+            test.end()
+          }
+        }
+        test.end()
+      })
+
+      await settlementsInitTest.test('add participant FSPIOP_CALLBACK_URL_TRANSFER_PUT endpoint', async test => {
+        let headers = {
+          'Content-Type': 'application/json'
+        }
+        for (let fsp = 0; fsp < fspList.length; fsp++) {
+          try {
+            let url = `${CENTRAL_LEDGER_ADMIN_URI_PREFIX}://${CENTRAL_LEDGER_ADMIN_HOST}:${CENTRAL_LEDGER_ADMIN_PORT}${CENTRAL_LEDGER_ADMIN_BASE}/participants/${fspList[fsp]}/endpoints`
+            let body = {
+              type: 'FSPIOP_CALLBACK_URL_TRANSFER_PUT',
+              value: `http://localhost:1080/${fspList[fsp]}/transfers/{{transferId}}`
+            }
+            let opts = {
+              method: 'POST',
+              headers,
+              body: JSON.stringify(body)
+            }
+            let res = await fetch(url, opts)
+            test.equal(res.status, 201, `returned 201 created endpoint for ${fspList[fsp]}`)
+          } catch (err) {
+            Logger.error(`settlementsInitTest failed with error - ${err}`)
+            test.fail()
+            test.end()
+          }
+        }
+        test.end()
+      })
+
+      await settlementsInitTest.test('create FSPIOP_CALLBACK_URL_TRANSFER_ERROR endpoint', async test => {
+        let headers = {
+          'Content-Type': 'application/json'
+        }
+        for (let fsp = 0; fsp < fspList.length; fsp++) {
+          try {
+            let url = `${CENTRAL_LEDGER_ADMIN_URI_PREFIX}://${CENTRAL_LEDGER_ADMIN_HOST}:${CENTRAL_LEDGER_ADMIN_PORT}${CENTRAL_LEDGER_ADMIN_BASE}/participants/${fspList[fsp]}/endpoints`
+            let body = {
+              type: 'FSPIOP_CALLBACK_URL_TRANSFER_ERROR',
+              value: `http://localhost:1080/${fspList[fsp]}//transfers/{{transferId}}/error`
+            }
+            let opts = {
+              method: 'POST',
+              headers,
+              body: JSON.stringify(body)
+            }
+            let res = await fetch(url, opts)
+            test.equal(res.status, 201, `returned 201 created endpoint for ${fspList[fsp]}`)
+          } catch (err) {
+            Logger.error(`settlementsInitTest failed with error - ${err}`)
+            test.fail()
+            test.end()
+          }
+        }
+        test.end()
       })
 
       await settlementsInitTest.end()
