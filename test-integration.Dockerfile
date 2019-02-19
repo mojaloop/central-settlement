5a3073bf45f6ed465750832c8bdfe74c39019784
ggrg <georgi.georgiev@modusbox.com>
2019-02-19 14:06:31 +0200

    Amendments to integration-runner

---
+++ test-integration.Dockerfile
@@ -0,0 +1,26 @@
+FROM mhart/alpine-node:10.15.1
+USER root
+
+FROM mhart/alpine-node:8.9.4
+
+WORKDIR /opt/central-settlement
+COPY config /opt/central-settlement/config
+COPY src /opt/central-settlement/src
+COPY test /opt/central-settlement/test
+COPY package.json /opt/central-settlement
+COPY README.md /opt/central-settlement
+
+# overwrite default.json with integration environment specific config
+RUN cp -f /opt/central-settlement/test/integration-config.json /opt/central-settlement/config/default.json
+
+RUN apk add --no-cache -t build-dependencies git make gcc g++ python libtool autoconf automake \
+    && cd $(npm root -g)/npm \
+    && npm config set unsafe-perm true
+
+RUN npm install --production && \
+  npm uninstall -g npm
+
+RUN apk del build-dependencies
+
+EXPOSE 3007
+CMD node src/server.js
--- test/integration-runner.sh
+++ test/integration-runner.sh
@@ -3,35 +3,36 @@
 >&2 echo "--==== Integration Tests Runner ====--"
 
 if [ $# -ne 1 ]; then
-    echo ""
-    echo "Usage: $0 {env-file}"
-    echo "{env-file} must contain the following variables:"
-    echo " - DOCKER_IMAGE: Name of Image"
-    echo " - DOCKER_TAG: Tag/Version of Image"
-    echo " - DOCKER_FILE: Recipe to be used for Docker build"
-    echo " - DOCKER_WORKING_DIR: Docker working directory"
-    echo " - DB_USER: Database user"
-    echo " - DB_PASSWORD: Database password"
-    echo " - DB_HOST: Database host name"
-    echo " - DB_PORT: Database container port"
-    echo " - DB_NAME: Database database"
-    echo " - DB_IMAGE: Docker Image for Database"
-    echo " - DB_TAG: Docker tag/version for Database"
-    echo " - KAFKA_IMAGE: Kafka image:tag"
-    echo " - KAFKA_HOST: Kafka host"
-    echo " - KAFKA_ZOO_PORT: Kafka host name"
-    echo " - KAFKA_BROKER_PORT: Kafka container port"
-    echo " - APP_HOST: Application host name"
-    echo " - APP_PORT: Application port"
-    echo " - APP_DIR_TEST_INTEGRATION: Location of the integration tests relative to the working directory"
-    echo " - APP_DIR_TEST_RESULTS: Location of test results relative to the working directory"
-    echo " - TEST_DIR: Base directory for tests"
-    echo " - TEST_RESULTS_FILE: Name of integration test results xml file"
-    echo " - TEST_CMD: Integration test command to be executed"
-    echo ""
-    echo " * IMPORTANT: Ensure you have the required env in the test/.env to execute the application"
-    echo ""
-    exit 1
+  ## TODO: update comments section
+  echo ""
+  echo "Usage: $0 {env-file}"
+  echo "{env-file} must contain the following variables:"
+  echo " - DOCKER_IMAGE: Name of Image"
+  echo " - DOCKER_TAG: Tag/Version of Image"
+  echo " - DOCKER_FILE: Recipe to be used for Docker build"
+  echo " - DOCKER_WORKING_DIR: Docker working directory"
+  echo " - DB_USER: Database user"
+  echo " - DB_PASSWORD: Database password"
+  echo " - DB_HOST: Database host name"
+  echo " - DB_PORT: Database container port"
+  echo " - DB_NAME: Database database"
+  echo " - DB_IMAGE: Docker Image for Database"
+  echo " - DB_TAG: Docker tag/version for Database"
+  echo " - KAFKA_IMAGE: Kafka image:tag"
+  echo " - KAFKA_HOST: Kafka host"
+  echo " - KAFKA_ZOO_PORT: Kafka host name"
+  echo " - KAFKA_BROKER_PORT: Kafka container port"
+  echo " - APP_HOST: Application host name"
+  echo " - APP_PORT: Application port"
+  echo " - APP_DIR_TEST_INTEGRATION: Location of the integration tests relative to the working directory"
+  echo " - APP_DIR_TEST_RESULTS: Location of test results relative to the working directory"
+  echo " - TEST_DIR: Base directory for tests"
+  echo " - TEST_RESULTS_FILE: Name of integration test results xml file"
+  echo " - TEST_CMD: Integration test command to be executed"
+  echo ""
+  echo " * IMPORTANT: Ensure you have the required env in the test/.env to execute the application"
+  echo ""
+  exit 1
 fi
 >&2 echo ""
 >&2 echo "====== Loading environment variables ======"
@@ -50,6 +51,12 @@ mkdir -p $TEST_DIR/results
 stop_docker() {
   >&1 echo "Kafka is shutting down $KAFKA_HOST"
   (docker stop $KAFKA_HOST && docker rm $KAFKA_HOST) > /dev/null 2>&1
+  >&1 echo "Simulator is shutting down $SIMULATOR_HOST"
+  (docker stop $SIMULATOR_HOST && docker rm $SIMULATOR_HOST) > /dev/null 2>&1
+  >&1 echo "Central-ledger is shutting down $CENTRAL_LEDGER_HOST"
+  (docker stop $CENTRAL_LEDGER_HOST && docker rm $CENTRAL_LEDGER_HOST) > /dev/null 2>&1
+  >&1 echo "Ml-api-adapter is shutting down $ML_API_ADAPTER_HOST"
+  (docker stop $ML_API_ADAPTER_HOST && docker rm $ML_API_ADAPTER_HOST) > /dev/null 2>&1
   >&1 echo "$DB_HOST environment is shutting down"
   (docker stop $DB_HOST && docker rm $DB_HOST) > /dev/null 2>&1
   >&1 echo "$APP_HOST environment is shutting down"
@@ -103,13 +110,13 @@ run_test_command() {
 }
 
 fcurl() {
-	docker run --rm -i \
-		--link $ENDPOINT_HOST \
-		--network $DOCKER_NETWORK \
-		--entrypoint curl \
-		"jlekie/curl:latest" \
-        --silent --head --fail \
-		"$@"
+  docker run --rm -i \
+    --link $ENDPOINT_HOST \
+    --network $DOCKER_NETWORK \
+    --entrypoint curl \
+    "jlekie/curl:latest" \
+    --silent --head --fail \
+    "$@"
 }
 
 # Kafka functions
@@ -136,7 +143,7 @@ fkafka() {
 	  --link $KAFKA_HOST \
 	  --network $DOCKER_NETWORK \
 	  --env KAFKA_HOST="$KAFKA_HOST" \
-    --env KAFKA_ZOO_PORT="$KAFKA_ZOO_PORT" \
+      --env KAFKA_ZOO_PORT="$KAFKA_ZOO_PORT" \
 	  taion809/kafka-cli \
 	  /bin/sh \
 	  -c \
@@ -179,10 +186,53 @@ is_db_up() {
   fdb 'mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASSWORD" -e "select 1"' > /dev/null 2>&1
 }
 
+# Mojaloop services
+
+start_simulator () {
+  docker run --rm -td \
+    -p 8444:8444
+    --network $DOCKER_NETWORK \
+    --name=$SIMULATOR_HOST \
+    --env TRANSFERS_ENDPOINT=http://host.docker.internal:3000
+    $SIMULATOR_IMAGE
+
+}
+
+is_simulator_up() {
+  fcurl "http://${SIMULATOR_HOST}:8444/health?"
+}
+
+start_central_ledger () {
+  docker run --rm -td \
+    -p 3001:3001
+    --network $DOCKER_NETWORK \
+    --name=$CENTRAL_LEDGER_HOST \
+    $CENTRAL_LEDGER_IMAGE
+
+}
+
+is_central_ledger_up() {
+  fcurl "http://${CENTRAL_LEDGER_HOST}:3001/health?"
+}
+
+start_ml_api_adapter () {
+  docker run --rm -td \
+    -p 3000:3000
+    --network $DOCKER_NETWORK \
+    --name=$ML_API_ADAPTER_HOST \
+    $ML_API_ADAPTER_IMAGE
+
+}
+
+is_ml_api_adapter_up() {
+  fcurl "http://${ML_API_ADAPTER_HOST}:3000/health?"
+}
+
 # Script execution
 
 stop_docker
 
+## TODO: move after central-ledger is up and db migration is finished?
 >&1 echo "Building Docker Image $DOCKER_IMAGE:$DOCKER_TAG with $DOCKER_FILE"
 docker build --no-cache -t $DOCKER_IMAGE:$DOCKER_TAG -f $DOCKER_FILE .
 
@@ -228,6 +278,7 @@ until is_db_up; do
   sleep 5
 done
 
+## TODO: run migrations from central-ledger host
 >&1 echo "Running migrations"
 ftest "npm run migrate"
 
@@ -237,6 +288,54 @@ then
   clean_docker
   exit 1
 fi
+
+>&1 echo "Simulator is starting"
+start_simulator
+
+if [ "$?" != 0 ]
+then
+  >&2 echo "Starting Simulator failed...exiting"
+  clean_docker
+  exit 1
+fi
+
+>&2 echo "Waiting for Simulator to start"
+until is_simulator_up; do
+  >&2 printf "."
+  sleep 5
+done
+
+>&1 echo "Central-ledger is starting"
+start_central_ledger
+
+if [ "$?" != 0 ]
+then
+  >&2 echo "Starting Central-ledger failed...exiting"
+  clean_docker
+  exit 1
+fi
+
+>&2 echo "Waiting for Central-ledger to start"
+until is_central_ledger_up; do
+  >&2 printf "."
+  sleep 5
+done
+
+>&1 echo "Ml-api-adapter is starting"
+start_ml_api_adapter
+
+if [ "$?" != 0 ]
+then
+  >&2 echo "Starting Ml-api-adapter failed...exiting"
+  clean_docker
+  exit 1
+fi
+
+>&2 echo "Waiting for Ml-api-adapter to start"
+until is_ml_api_adapter_up; do
+  >&2 printf "."
+  sleep 5
+done
 
 >&1 echo "Integration tests are starting"
 run_test_command
