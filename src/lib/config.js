efa9df671e2ec7ce8e260df43f42737d5e2639ae
Claudio Viola <claudio-viola@users.noreply.github.com>
2020-09-11 16:22:27 +0200

    Updating circle ci pipeline

--- src/lib/config.js
+++ src/lib/config.js
@@ -15,26 +15,26 @@ module.exports = {
     pool: {
       log: (message, logLevel) => console.log(`${logLevel}: ${message}`)
 
-    //   // minimum size
-    //   min: RC.DATABASE.POOL_MIN_SIZE,
-    //   // maximum size
-    //   max: RC.DATABASE.POOL_MAX_SIZE,
-    //   // acquire promises are rejected after this many milliseconds
-    //   // if a resource cannot be acquired
-    //   acquireTimeoutMillis: RC.DATABASE.ACQUIRE_TIMEOUT_MILLIS,
-    //   // create operations are cancelled after this many milliseconds
-    //   // if a resource cannot be acquired
-    //   createTimeoutMillis: RC.DATABASE.CREATE_TIMEOUT_MILLIS,
-    //   // destroy operations are awaited for at most this many milliseconds
-    //   // new resources will be created after this timeout
-    //   destroyTimeoutMillis: RC.DATABASE.DESTROY_TIMEOUT_MILLIS,
-    //   // free resouces are destroyed after this many milliseconds
-    //   idleTimeoutMillis: RC.DATABASE.IDLE_TIMEOUT_MILLIS,
-    //   // how often to check for idle resources to destroy
-    //   reapIntervalMillis: RC.DATABASE.REAP_INTERVAL_MILLIS,
-    //   // long long to idle after failed create before trying again
-    //   createRetryIntervalMillis: RC.DATABASE.CREATE_RETRY_INTERVAL_MILLIS
-    //   // ping: function (conn, cb) { conn.query('SELECT 1', cb) }
+      // minimum size
+      min: RC.DATABASE.POOL_MIN_SIZE,
+      // maximum size
+      max: RC.DATABASE.POOL_MAX_SIZE,
+      // acquire promises are rejected after this many milliseconds
+      // if a resource cannot be acquired
+      acquireTimeoutMillis: RC.DATABASE.ACQUIRE_TIMEOUT_MILLIS,
+      // create operations are cancelled after this many milliseconds
+      // if a resource cannot be acquired
+      createTimeoutMillis: RC.DATABASE.CREATE_TIMEOUT_MILLIS,
+      // destroy operations are awaited for at most this many milliseconds
+      // new resources will be created after this timeout
+      destroyTimeoutMillis: RC.DATABASE.DESTROY_TIMEOUT_MILLIS,
+      // free resouces are destroyed after this many milliseconds
+      idleTimeoutMillis: RC.DATABASE.IDLE_TIMEOUT_MILLIS,
+      // how often to check for idle resources to destroy
+      reapIntervalMillis: RC.DATABASE.REAP_INTERVAL_MILLIS,
+      // long long to idle after failed create before trying again
+      createRetryIntervalMillis: RC.DATABASE.CREATE_RETRY_INTERVAL_MILLIS
+      // ping: function (conn, cb) { conn.query('SELECT 1', cb) }
     },
     debug: RC.DATABASE.DEBUG
   },
