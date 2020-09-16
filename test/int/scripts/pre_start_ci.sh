33149a1c7b58b058f641afda72ef80078bb9ce80
Claudio Viola <claudio-viola@users.noreply.github.com>
2020-09-16 22:36:14 +0200

    Adding docker layers cache

--- test/int/scripts/pre_start_ci.sh
+++ test/int/scripts/pre_start_ci.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 mydir="${0%/*}"
 sh "$mydir"/cp_migrations_ci.sh
-
-docker-compose --env-file ./test/int/docker/.env -f docker-compose.integration_2.yml up -d --build
+docker-compose -p $CIRCLE_PROJECT_REPONAME --env-file ./test/int/docker/.env -f docker-compose.integration_2.yml up -d --build
 
 sh "$mydir"/wait_for_mysql.sh
 
 # docker-compose -f docker-compose.integration_2.yml down
+# https://blog.jondh.me.uk/2018/04/strategies-for-docker-layer-caching-in-circleci/
