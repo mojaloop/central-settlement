163caa884f10736f774d00fbd8c8bb4264b2ddf0
Claudio Viola <claudio-viola@users.noreply.github.com>
2020-09-16 21:15:33 +0200

    Using branch specific migrations

---
+++ test/int/scripts/cp_migrations_ci.sh
@@ -0,0 +1,11 @@
+BRANCH='feature/OTC-816-additional-ledger-account-initial-setup'
+TMP_FOLDER='tmp/central-ledger'
+if ! [  -d $TMP_FOLDER ]
+then
+ rm -rf $TMP_FOLDER
+ git clone --depth 1 --single-branch --branch $BRANCH git@github.com:mojaloop/central-ledger.git $TMP_FOLDER
+ mkdir -p ./migrations ./seeds
+ rm -rf ./migrations/* ./seeds/*
+ mv $TMP_FOLDER/migrations/* ./migrations
+ mv $TMP_FOLDER/seeds/* ./seeds
+fi
--- test/int/scripts/pre_start_ci.sh
+++ test/int/scripts/pre_start_ci.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mydir="${0%/*}"
+sh "$mydir"/cp_migrations_ci.sh
 
 docker-compose --env-file ./test/int/docker/.env -f docker-compose.integration_2.yml up -d --build
-sh "$mydir"/cp_migrations.sh
 
 sh "$mydir"/wait_for_mysql.sh
 
