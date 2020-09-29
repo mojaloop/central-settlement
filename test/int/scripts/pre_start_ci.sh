6f8689e7a11551312a644f1d333969fcd391a80e
Claudio Viola <claudio-viola@users.noreply.github.com>
2020-09-29 22:36:28 +0200

    Updating gitlab ci

--- test/int/scripts/pre_start_ci.sh
+++ test/int/scripts/pre_start_ci.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 mydir="${0%/*}"
 sh "$mydir"/cp_migrations_ci.sh
-docker-compose -p $CIRCLE_PROJECT_REPONAME --env-file ./test/int/docker/.env -f docker-compose.integration_2.yml up -d --build
+if [  -z  "$CIRCLE_PROJECT_REPONAME" ]
+then
+  docker-compose -p $CIRCLE_PROJECT_REPONAME --env-file ./test/int/docker/.env -f docker-compose.integration_2.yml up -d --build
+else
+  docker-compose --env-file ./test/int/docker/.env -f docker-compose.integration_2.yml up -d --build
+fi
 
 sh "$mydir"/wait_for_mysql.sh
 
