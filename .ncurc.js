cad09aefdf56dfa94c1927f10cc36ee7afdee370
Kevin <kleyow@gmail.com>
2022-07-13 07:57:17 -0500

    chore: address comments

--- .ncurc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  reject: [
-    // From v6 get-port is now a pure esm package.
-    "get-port",
-    // @hapi/catbox-memory v6 and onwards breaks tests. Needs investigation.
-    "@hapi/catbox-memory"
-  ]
-}
