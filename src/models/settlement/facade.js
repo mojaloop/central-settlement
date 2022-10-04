fbd0d4db846e1bb73f2fdf5ae9c85f969f878e50
koekiebox <jason@koekiebox.com>
2022-10-04 11:17:01 +0200

    Circle-CI fixes.

--- src/models/settlement/facade.js
+++ src/models/settlement/facade.js
@@ -43,7 +43,7 @@ const Logger = require('@mojaloop/central-services-logger')
 const util = require('util')
 
 const GREEN = '\x1b[32m%s\x1b[0m'
-const YELLOW = '\x1b[33m%s\x1b[5m'
+const YELLOW = '\x1b[33m%s\x1b[0m'
 const BLUE = '\x1b[34m%s\x1b[0m'
 const MAGENTA = '\x1b[35m%s\x1b[0m'
 const TURQUOISE = '\x1b[36m%s\x1b[0m'
