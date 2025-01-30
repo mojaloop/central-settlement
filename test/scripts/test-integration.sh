6300ed6a8156ea351db7bcc88ef582899905bd37
Kalin Krustev <kalin.krustev@gmail.com>
2025-01-30 10:39:33 +0000

    test: restore integration tests

---
+++ test/scripts/test-integration.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xe
+
+docker load -i /tmp/docker-image.tar
+docker compose up -d
+docker compose ps
+
+npm run wait-4-docker
+
+curl localhost:3000/health && npm -s run test:int
+
+docker compose down -v --timeout 30
+docker compose -f ./docker-compose.yml -f ./docker-compose.pm4ml.yml up -d
+docker compose ps
